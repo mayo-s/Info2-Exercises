e7667f55fe1af389eb9e43b0485996d81cb6587c
Mario Schuetz <mayo2@gmx.de>
2016-04-27 22:05:10 +0200

    Initial commit of Histogram Exercise

---
+++ Histogram/src/Character.java
@@ -0,0 +1,23 @@
+
+public class Character {
+
+	char aChar;
+	int cnt;
+
+	public Character(char aChar, int cnt) {
+		this.aChar = aChar;
+		this.cnt = cnt;
+	}
+
+	public char getChar() {
+		return aChar;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int add) {
+		this.cnt = add;
+	}
+}
--- Histogram/src/Histogram.java
+++ Histogram/src/Histogram.java
@@ -1,29 +1,59 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Histogram {
 
-	
-	public static void main(String[] args) throws IOException{
-		
-			reader();
-	}
-	
-	public static void reader(){
-				
-		FileReader input;
-		FileWriter output;
-		
-		try{
-			input = new FileReader("C:\\Users\\mayo\\Dropbox\\Studium\\S2\\INFO2\\Exercises\\Histogram\\src\\aFile.txt");		
-			int cnt;
-			while((cnt = input.read()) != -1){
-				System.out.print((char)cnt);
-			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		
+	Character aChar;
+	static ArrayList<Character> histogram;
+
+	public static void main(String[] args) throws IOException {
+		reader();
+
 	}
 
+	public Histogram() {
+		histogram = new ArrayList<Character>();
+	}
+
+	public static void reader() {
+		FileReader input;
+		FileWriter output;
+		try {
+			input = new FileReader(
+					"C:\\Users\\mayo\\Dropbox\\Studium\\S2\\INFO2\\Exercises\\Histogram\\src\\aFile.txt");
+			int nextChar;
+			while ((nextChar = input.read()) != -1) {
+				// check if character is a letter
+				if ((nextChar >= 65 && nextChar <= 90) || (nextChar >= 97 && nextChar <= 122)) {
+					System.out.print((char) nextChar);
+					histogram(nextChar);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void histogram(int tmpChar) {
+		int charCnt = 0;
+		int i = 0;
+		boolean counted = false;
+
+		// check if character is already in arraylist
+		for(Character aChar : histogram){
+				if (tmpChar == histogram.get(i).getChar()) {
+					int newCnt = histogram.get(i).getCnt() + 1;
+					histogram.get(i).setCnt(newCnt);
+				}
+			
+				histogram.add(new Character((char) tmpChar, charCnt));
+			
+		}
+	}
+
+	public void printHistogram() {
+		for (Character aChar : histogram) {
+			System.out.println(aChar + ": ");
+		}
+	}
 }
