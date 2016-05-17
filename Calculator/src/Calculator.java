216cc37d882368233644e8514fff408aa914b9c4
Mario.Schuetz <mayo2@gmx.de>
2016-05-17 10:39:34 +0200

    added project Calculator by Barnes/Kolling. implemented Multiplication/Divison functionality

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,37 @@
+/**
+ * The main class of a simple calculator. Create one of these and you'll
+ * get the calculator on screen.
+ * 
+ * @author David J. Barnes and Michael Kolling
+ * @version 2008.03.30
+ * 
+ * edited by Mario Schuetz & Kevin Klaus
+ * 
+ */
+public class Calculator
+{
+    private CalcEngine engine;
+    private UserInterface gui;
+    
+    public static void main(String[] args){
+    	Calculator calc = new Calculator();
+    	calc.show();
+    }
+
+    /**
+     * Create a new calculator and show it.
+     */
+    public Calculator()
+    {
+        engine = new CalcEngine();
+        gui = new UserInterface(engine);
+    }
+
+    /**
+     * In case the window was closed, show it again.
+     */
+    public void show()
+    {
+        gui.setVisible(true);
+    }
+}
