3b35c88729ede756241bbf2aaa30c9456b3b904d
Mario Schuetz <mayo2@gmx.de>
2016-05-22 13:29:31 +0200

    modify HexEngine

--- Calculator/src/HexEngine.java
+++ Calculator/src/HexEngine.java
@@ -1,16 +1,32 @@
+public class HexEngine extends CalcEngine {
+	// HEX button pressed?
+	private boolean hex;
 
-public class HexEngine extends CalcEngine{
-	
-	private int hex2int(String hexValue){
-		
-		int hex2int = Integer.parseInt(hexValue, 16);		
-		
-		return hex2int;
+	public HexEngine() {
+		HexEngine hex = new HexEngine();
 	}
-	
-	private String int2hex(int intValue){
-		String int2hex = Integer.toHexString(intValue);
-		return int2hex;
+	// intBack = toInt(hex);
+	// hexBack = toHex(in);
+
+	// returns int in as a string representing a the appropriate hex number or
+	// "number too large" if not applicable
+	private String toHex(int integerHex) {
+		String hex = "number too large";
+		hex = Integer.toHexString(integerHex);
+		return hex;
 	}
 
+	// returns string hex as an integer or -1 if not applicable
+	private int toInt(String hexString) {
+		int integerHex = -1;
+		// in = Integer.decode(hexString);
+		return integerHex;
+	}
+
+	public void activate() {
+		if (!hex)
+			hex = true;
+		else
+			hex = false;
+	}
 }
